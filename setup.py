a22f370998bf0830114882cc222899750dd613b7
pear-c <100738513+pear-c@users.noreply.github.com>
2022-05-04 09:52:21 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup (
   include_package_data = True,
   name='mycalc123',
-  version='1.0.0',
+  version='1.0.1',
   description='oss-dev my calculator 0001',
   author='tjdghks',
   author_email='zxcv9676@naver.com',
